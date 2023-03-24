0a8e66058b663efd60ecbe8bf90e3fd0935f6df0
BorisCamarena <128287239+BorisCamarena@users.noreply.github.com>
2023-03-23 18:05:13 -0600

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,7 +5,7 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameAI, Direction, Point
+from juego1 import ViboritaInteligente, Direccion, Punto
 from model import Linear_QNet, QTrainer
 from helper import plot
 
